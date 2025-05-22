cb7e806724f0cd976e720a86f8ea6946d703264c
Hector Fernandes <hectorhu17@gmail.com>
2025-05-22 12:28:39 -0300

    test: create a wallet test

---
+++ src/test/java/com/devhector/wallet/model/WalletTest.java
@@ -0,0 +1,47 @@
+package com.devhector.wallet.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+import com.devhector.wallet.model.exceptions.InsufficientBalanceException;
+import com.devhector.wallet.model.exceptions.NegativeAmountException;
+
+public class WalletTest {
+  private static final BigDecimal BALANCE = new BigDecimal(100);
+
+  @Test
+  void whenAmountIsNegativeShouldThrowNegativeAmountException() {
+    Wallet wallet = new Wallet(BALANCE);
+    BigDecimal amount = new BigDecimal(-10);
+
+    assertThrows(NegativeAmountException.class, () -> wallet.addFunds(amount));
+    assertThrows(NegativeAmountException.class, () -> wallet.deductFunds(amount));
+  }
+
+  @Test
+  void whenAmountIsPositiveAndAddFundsShouldAddAmountInBalance() {
+    Wallet wallet = new Wallet(BALANCE);
+    BigDecimal amount = new BigDecimal(10);
+
+    wallet.addFunds(amount);
+
+    assertEquals(new BigDecimal(110), wallet.balance());
+  }
+
+  @Test
+  void whenDeductFundsAndAmountIsGreaterThanBalanceShouldThrowsInsufficientBalanceException() {
+    Wallet wallet = new Wallet(BALANCE);
+    BigDecimal amount = new BigDecimal(110);
+
+    assertThrows(InsufficientBalanceException.class, () -> wallet.deductFunds(amount));
+  }
+
+  @Test
+  void whenDeductFundsThenSubtractBalance() {
+
+  }
+}
