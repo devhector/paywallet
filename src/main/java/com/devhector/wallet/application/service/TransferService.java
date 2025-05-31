4420e5ef3f5443fa155a384304294249e8b840f1
Hector Fernandes <hectorhu17@gmail.com>
2025-05-31 16:35:55 -0300

    feat: add logger

--- src/main/java/com/devhector/wallet/application/service/TransferService.java
+++ src/main/java/com/devhector/wallet/application/service/TransferService.java
@@ -2,17 +2,21 @@ package com.devhector.wallet.application.service;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devhector.wallet.domain.model.Transaction;
 import com.devhector.wallet.domain.model.Transfer;
 import com.devhector.wallet.domain.model.User;
+import com.devhector.wallet.domain.model.exceptions.UserNotFoundException;
 import com.devhector.wallet.domain.repository.TransactionRepository;
 import com.devhector.wallet.domain.repository.UserRepository;
 
 @Service
 public class TransferService {
+  private static final Logger logger = LoggerFactory.getLogger(TransferService.class);
   private final UserRepository userRepository;
   private final TransactionRepository transactionRepository;
 
@@ -23,14 +27,23 @@ public class TransferService {
 
   @Transactional
   public Transaction tranfer(String payerCpfCnpj, String payeeCpfCnpj, BigDecimal amount) {
-    User payer = userRepository.findByCpfCnpj(payerCpfCnpj);
-    User payee = userRepository.findByCpfCnpj(payeeCpfCnpj);
+    Transaction transaction;
 
-    Transaction transaction = Transfer.execute(payer, payee, amount);
+    try {
+      User payer = userRepository.findByCpfCnpj(payerCpfCnpj).orElseThrow(
+          () -> new UserNotFoundException("Payer not found with id " + payeeCpfCnpj));
+      User payee = userRepository.findByCpfCnpj(payeeCpfCnpj).orElseThrow(
+          () -> new UserNotFoundException("Payee not found with id " + payeeCpfCnpj));
 
-    userRepository.save(payer);
-    userRepository.save(payee);
-    transactionRepository.save(transaction);
+      transaction = Transfer.execute(payer, payee, amount);
+
+      userRepository.save(payer);
+      userRepository.save(payee);
+      transactionRepository.save(transaction);
+    } catch (UserNotFoundException e) {
+      logger.error("Tranfer failed: {}", e.getMessage(), e);
+      throw e;
+    }
 
     return transaction;
   }
